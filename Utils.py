46d17acdcf363a94d7bbd751e84149561265cdd6
rinkwitz <philip.rinkwitz@protonmail.com>
2020-03-02 11:35:40 +0100

    updates

--- Utils.py
+++ Utils.py
@@ -4,9 +4,7 @@ import matplotlib.pyplot as plt
 from scipy.stats import *
 
 def unif_distr_exp_decaying_tails_pdf(y, mu, delta, sigma_1, sigma_2):
-    C = np.sqrt(2 * np.pi * sigma_1 ** 2) / 2 + \
-        np.sqrt(2 * np.pi * sigma_2 ** 2) / 2 + \
-        2 * delta
+    C = np.sqrt(2 * np.pi * sigma_1 ** 2) / 2 + np.sqrt(2 * np.pi * sigma_2 ** 2) / 2. + 2 * delta
     if y < mu - delta:
         return np.exp(-(y - (mu - delta)) ** 2 / (2 * sigma_1 ** 2)) / C
     elif mu - delta <= y and y <= delta + mu:
@@ -19,15 +17,36 @@ def trial_proposal(y, x, j, M, delta=2., delta_1=2., sigma=.05, sigma_0=3., sigm
     if j == 0:
         return unif_distr_exp_decaying_tails_pdf(y, x, delta_1, sigma, sigma)
     elif j < M - 1:
-        return (unif_distr_exp_decaying_tails_pdf(y, x - delta_1 - delta, delta, sigma, sigma) + \
-                unif_distr_exp_decaying_tails_pdf(y, x + delta_1 + delta, delta, sigma, sigma)) / 2
+        return (unif_distr_exp_decaying_tails_pdf(y, x - j * (delta_1 + delta), delta, sigma, sigma) + unif_distr_exp_decaying_tails_pdf(y, x + j * (delta_1 + delta), delta, sigma, sigma)) / 2
     else:
-        return (unif_distr_exp_decaying_tails_pdf(y, x - delta_1 - delta, delta, sigma_0, sigma) + \
-                unif_distr_exp_decaying_tails_pdf(y, x + delta_1 + delta, delta, sigma, sigma_1)) / 2
+        return (unif_distr_exp_decaying_tails_pdf(y, x - j * (delta_1 + delta), delta, sigma_0, sigma) + unif_distr_exp_decaying_tails_pdf(y, x + j * (delta_1 + delta), delta, sigma, sigma_1)) / 2
 
 def rejection_sampling_trial(x, j, M):
-    while True:
-        x2 = np.random.uniform()
-        y2 = np.random.uniform()
-        if trial_proposal(x2, x, j, M) < y2:
-            return x2
+    if j == 0:
+        upper_bound = np.max([trial_proposal(p, x, j, M)/norm(loc=x, scale=2.).pdf(p) for p in np.linspace(x - 4, x + 4, 100)])
+        while True:
+            Y = norm(loc=x, scale=2.).rvs()
+            U = np.random.uniform()
+            if U < trial_proposal(Y, x, j, M) / (upper_bound * norm(loc=x, scale=2.).pdf(Y)):
+                return Y
+    elif j < M - 1:
+        upper_bound = np.max(
+            [trial_proposal(p, x, j, M) / (norm(loc=x, scale=2.).pdf(p) / 2.) for p in np.linspace(x - (j+1)*4, x - (j-1) * 4, 100)])
+        while True:
+            U = np.random.uniform()
+            if np.random.rand() < .5:
+                Y = norm(loc=x - 4 * j, scale=2.).rvs()
+                if U < trial_proposal(Y, x, j, M) / (upper_bound * norm(loc=x - 4 * j, scale=2.).pdf(Y)):
+                    return Y
+            else:
+                Y = norm(loc=x+4*j, scale=2.).rvs()
+                if U < trial_proposal(Y, x, j, M) / (upper_bound * norm(loc=x + 4 * j, scale=2.).pdf(Y)):
+                    return Y
+    else:
+        upper_bound = (1 / (np.sqrt(2 * np.pi * .05 ** 2) / 2 + np.sqrt(2 * np.pi * 3. ** 2) / 2. + 2 * 2.)) / 2.
+    #while True:
+    #    Y = np.random.uniform(x - j * 4, x + j * 4)
+    #    U = np.random.uniform()
+    #    if U < trial_proposal(Y, x, j, M) / (upper_bound ):
+    #        return Y
+        #print(Y, x, j, M, trial_proposal(Y, x, j, M))
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 from Utils import *
 
-M = 10
+M = 5
 N = 100
 
 x = np.random.randn(4)
@@ -13,3 +13,12 @@ for n in range(N):
         # 5
 
         print(z)
+        for j in range(M):
+            xs = np.linspace(x[k] - 30, x[k] + 30, 1000)
+            ys = [trial_proposal(i, x[k], j, M) for i in xs]
+            z = [rejection_sampling_trial(x[k], j, M) for j2 in range(20)]
+            plt.plot(xs, ys)
+            plt.scatter(z, np.zeros(len(z)))
+            print(z)
+        plt.show()
+        break
