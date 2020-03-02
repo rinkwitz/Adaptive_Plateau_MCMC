53505734966dac277c92516078530c06fe5f6b68
rinkwitz <philip.rinkwitz@protonmail.com>
2020-03-02 12:06:32 +0100

    updates

--- Utils.py
+++ Utils.py
@@ -22,31 +22,50 @@ def trial_proposal(y, x, j, M, delta=2., delta_1=2., sigma=.05, sigma_0=3., sigm
         return (unif_distr_exp_decaying_tails_pdf(y, x - j * (delta_1 + delta), delta, sigma_0, sigma) + unif_distr_exp_decaying_tails_pdf(y, x + j * (delta_1 + delta), delta, sigma, sigma_1)) / 2
 
 def rejection_sampling_trial(x, j, M):
-    if j == 0:
-        upper_bound = np.max([trial_proposal(p, x, j, M)/norm(loc=x, scale=2.).pdf(p) for p in np.linspace(x - 4, x + 4, 100)])
-        while True:
-            Y = norm(loc=x, scale=2.).rvs()
-            U = np.random.uniform()
-            if U < trial_proposal(Y, x, j, M) / (upper_bound * norm(loc=x, scale=2.).pdf(Y)):
+    # if j == 0:
+    #     upper_bound = np.max([trial_proposal(p, x, j, M)/norm(loc=x, scale=2.).pdf(p) for p in np.linspace(x - 4, x + 4, 100)])
+    #     while True:
+    #         Y = norm(loc=x, scale=2.).rvs()
+    #         U = np.random.uniform()
+    #         #print(U, trial_proposal(Y, x, j, M) / (upper_bound * norm(loc=x, scale=2.).pdf(Y)))
+    #         if U < trial_proposal(Y, x, j, M) / (upper_bound * norm(loc=x, scale=2.).pdf(Y)):
+    #             return Y
+    # elif j < M - 1:
+    #     upper_bound = np.max(
+    #         [trial_proposal(p, x, j, M) / (norm(loc=x - 4*j, scale=2.).pdf(p) / 2.) for p in np.linspace(x - (j+1)*4, x - (j-1) * 4, 100)])
+    #     while True:
+    #         U = np.random.uniform()
+    #         if np.random.rand() < .5:
+    #             Y = norm(loc=x - 4. * j, scale=2.).rvs()
+    #             if U < trial_proposal(Y, x, j, M) / (upper_bound * norm(loc=x - 4. * j, scale=2.).pdf(Y) / 2.):
+    #                 return Y
+    #         else:
+    #             Y = norm(loc=x + 4. * j, scale=2.).rvs()
+    #             if U < trial_proposal(Y, x, j, M) / (upper_bound * norm(loc=x + 4. * j, scale=2.).pdf(Y) / 2.):
+    #                 return Y
+    # else:
+    #     upper_bound = np.max(
+    #         [trial_proposal(p, x, j, M) / (norm(loc=x - 4 * j, scale=3.).pdf(p) / 2.) for p in
+    #          np.linspace(x - (j + 3) * 4, x - (j - 3) * 4, 100)])
+    #     while True:
+    #         U = np.random.uniform()
+    #         if np.random.rand() < .5:
+    #             Y = norm(loc=x - 4. * j, scale=3.).rvs()
+    #             if U < trial_proposal(Y, x, j, M) / (upper_bound * norm(loc=x - 4. * j, scale=3.).pdf(Y) / 2.):
+    #                 return Y
+    #         else:
+    #             Y = norm(loc=x + 4. * j, scale=3.).rvs()
+    #             if U < trial_proposal(Y, x, j, M) / (upper_bound * norm(loc=x + 4. * j, scale=3.).pdf(Y) / 2.):
+    #                 return Y
+    upper_bound = np.max(
+        [trial_proposal(p, x, j, M) / (norm(loc=x - 4*j, scale=5.).pdf(p) / 2.) for p in np.linspace(x - (j+1)*4, x - (j-1) * 4, 100)])
+    while True:
+        U = np.random.uniform()
+        if np.random.rand() < .5:
+            Y = norm(loc=x - 4. * j, scale=5.).rvs()
+            if U < trial_proposal(Y, x, j, M) / (upper_bound * norm(loc=x - 4. * j, scale=5.).pdf(Y) / 2.):
                 return Y
-    elif j < M - 1:
-        upper_bound = np.max(
-            [trial_proposal(p, x, j, M) / (norm(loc=x, scale=2.).pdf(p) / 2.) for p in np.linspace(x - (j+1)*4, x - (j-1) * 4, 100)])
-        while True:
-            U = np.random.uniform()
-            if np.random.rand() < .5:
-                Y = norm(loc=x - 4 * j, scale=2.).rvs()
-                if U < trial_proposal(Y, x, j, M) / (upper_bound * norm(loc=x - 4 * j, scale=2.).pdf(Y)):
-                    return Y
-            else:
-                Y = norm(loc=x+4*j, scale=2.).rvs()
-                if U < trial_proposal(Y, x, j, M) / (upper_bound * norm(loc=x + 4 * j, scale=2.).pdf(Y)):
-                    return Y
-    else:
-        upper_bound = (1 / (np.sqrt(2 * np.pi * .05 ** 2) / 2 + np.sqrt(2 * np.pi * 3. ** 2) / 2. + 2 * 2.)) / 2.
-    #while True:
-    #    Y = np.random.uniform(x - j * 4, x + j * 4)
-    #    U = np.random.uniform()
-    #    if U < trial_proposal(Y, x, j, M) / (upper_bound ):
-    #        return Y
-        #print(Y, x, j, M, trial_proposal(Y, x, j, M))
+        else:
+            Y = norm(loc=x + 4. * j, scale=5.).rvs()
+            if U < trial_proposal(Y, x, j, M) / (upper_bound * norm(loc=x + 4. * j, scale=5.).pdf(Y) / 2.):
+                return Y
--- main.py
+++ main.py
@@ -8,17 +8,16 @@ x = np.random.randn(4)
 for n in range(N):
     for k in range(len(x)):
         # 4
-        z = [rejection_sampling_trial(x[k], j, M) for j in range(M)]
+        #z = [rejection_sampling_trial(x[k], j, M) for j in range(M)]
 
         # 5
 
-        print(z)
         for j in range(M):
+            print(j)
             xs = np.linspace(x[k] - 30, x[k] + 30, 1000)
             ys = [trial_proposal(i, x[k], j, M) for i in xs]
             z = [rejection_sampling_trial(x[k], j, M) for j2 in range(20)]
             plt.plot(xs, ys)
             plt.scatter(z, np.zeros(len(z)))
-            print(z)
         plt.show()
         break
